5716629ad073164fc6fefb1357e61277c0043810
AnNgo98 <40728998+AnNgo98@users.noreply.github.com>
2019-06-08 12:02:48 +0700

    a

--- QLThucDon/QLThucDon/BusinessLogicLayer/ThucDonBAL.cs
+++ QLThucDon/QLThucDon/BusinessLogicLayer/ThucDonBAL.cs
@@ -11,23 +11,22 @@ namespace BusinessLogicLayer
 {
     public class ThucDonBAL
     {
-        //private DAL db;
+        private DAL db;
 
-        //public ThucDonBAL()
-        //{
-        //    db = new DAL();
-        //}
+        public ThucDonBAL()
+        {
+            db = new DAL();
+        }
 
-        public string ThemThucDon(DateTime ngaytao,DateTime ngaylap,int sosuatan,string idMon,string idBua)
+        public string ThemThucDon(DateTime ngaytao,DateTime ngaylap,int sosuatan,int idMon)
         {
             string query = "spThemThucDon";
             //return db.ExecuteQuery(query, CommandType.StoredProcedure, null);
-            return DataProvider.Ins.db.ExecuteNonQuery(query, CommandType.StoredProcedure,
+            return db.ExecuteNonQuery(query, CommandType.StoredProcedure,
                 new SqlParameter("@NgayLap", ngaytao),
                 new SqlParameter("@NgayThucHien", ngaylap),
                 new SqlParameter("@SoPhanAn", sosuatan),
-                new SqlParameter("@IDMonAn", idMon),
-                new SqlParameter("@IDBua", idBua));
+                new SqlParameter("@IDMonAn", idMon));
         }
     }
 }
